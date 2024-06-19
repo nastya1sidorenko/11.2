48cb6062248b7d1fc410c5eb651393b13404587c
Nastya Sidorenko <stasia.sidorenko@gmail.com>
2024-06-19 23:05:06 +0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def capitalize_string(input_string):
+    """
+    Преобразует все символы во входной строке в верхний регистр.
+
+    Args:
+        input_string (str): Входная строка, которую нужно преобразовать.
+
+    Returns:
+        str: Строка в верхнем регистре.
+    """
     return input_string.upper()
