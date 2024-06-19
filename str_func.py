f9cf35c8ae3de5efcf44d0823a39258c87b79611
nastya_lynx <stasia.sidornko@gmail.com>
2024-06-19 23:11:16 +0400

    Добавлена вторая функция capitalize_words

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def capitalize_string(input_string):
     return input_string.upper()
+def capitalize_words(input_string):
+    """
+    Преобразует первую букву каждого слова во входной строке в верхний регистр.
+
+    Args:
+        input_string (str): Входная строка, содержащая слова.
+
+    Returns:
+        str: Строка с заглавными первыми буквами слов.
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
